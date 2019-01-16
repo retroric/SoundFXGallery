49acd1ff05a080ce501421e38c0c2d360f648973
Laurent Den <laurentd75@yahoo.fr>
2019-01-16 03:08:21 +0100

    v0.2-WIP - some bugs

--- effects.s
+++ effects.s
@@ -10,87 +10,187 @@
 ;;     d'après un post sur le forum oric.org par LadyWasky
 ;      cf. http://forums.oric.org/t638-musique-en-c#8085
 ;
-;      Version: v0.1
-;      Date: 15/01/19
+;      Version: v0.2
+;      Date: 16/01/19
 :       
 ;      Historique:
 ;      -----------------------------------------------------------------------------------------
-;      v0.1    - première version
+;      v0.2WIP - BUGS !!! 
 ;  =============================================================================================
  
 ; ---------------------- Données des effets sonores --------------------------------------------
-metalcrush_fxtable
+; 
+; Note: pour qu'un label soit exposé et accessible depuis un programme C linké avec le source ASM,
+; le label doit être précédé d'un caractère de soulignement. Dans le code C, pour référencer ce label,
+; on devra utiliser le nom du label SANS le caractère de soulignement initial
+
+;;;.data	;;; <--- cause des erreurs, les sons ne fonctionnent plus ??!!
+
+_metalcrush_fxtable
 	.byt 32,134,250,96,0,0,0,0,0,0,31,7,16,16
-electricity_fxtable
+_electricity_fxtable
     .byt 0,4,0,184,0,3,120,190,12,0,0,0,167,194
-helicopter_fxtable
+_helicopter_fxtable
     .byt 168,191,0,3,184,191,14,0,0,0,167,194,76,176
-helicopter2_fxtable
+_helicopter2_fxtable
     .byt 206,108,231,36,137,112,70,182,170,239,83,246,12,165
-distant_helicopter_fxtable
+_distant_helicopter_fxtable
     .byt 14,136,132,12,140,0,69,173,0,5,208,4,169,192
-engine_fxtable
+_engine_fxtable
     .byt 1,5,193,7,7,8,7,8,53,151,47,151,0,152
-planecockpit_fxtable
+_planecockpit_fxtable
     .byt 150,0,152,46,4,255,255,112,7,80,0,102,5,65
-turboprop_fxtable
+_turboprop_fxtable
     .byt 32,2,140,0,5,132,14,136,132,12,140,0,69,173
-pong_fxtable
+_pong_fxtable
     .byt $EE, $02, $00, $00, $00, $00, $00, $3E, $10, $00, $00, $D0, $07, $00
-pchh_fxtable
+_pchh_fxtable
     .byt $00, $00, $00, $00, $00, $00, $01, $37, $10, $00, $00, $D6, $0B, $00
 
+;;;.align 2
+_fx_data_vector_table:
+	.word _metalcrush_fxtable, _electricity_fxtable
+	.word _helicopter_fxtable, _helicopter2_fxtable
+	.word _distant_helicopter_fxtable, _engine_fxtable
+	.word _planecockpit_fxtable, _turboprop_fxtable
+	.word _pong_fxtable, _pchh_fxtable
+
+	
 ; ---------------------- Routines de lecture en ASM --------------------------------------------
+;;;.text
+
+_play_soundfx2
+	; routine ASM déclarée en C comme: extern void  play_soundfx2(const char *psg_data);
+	; NB: pour les appels de fonction le compilateur C passe les paramètres sur la pile
+	; => il faut dépiler l'adresse "HHLL" passée sur la pile
+	; normalement (sp) doit contenir HH et (sp+1) doit contenir LL.
+	ldy #0
+	lda (sp),y
+	sta $14
+	tax
+	iny
+
+	lda (sp),y
+	sta $15
+
+	php
+	sei
+	ldy #0 ; indiquer paramètre 0
+	jmp load_psg_param_value
+
+
+; routine de lecture générique
+; Entrée: X: poids fort adresse données sonores (14 octets pour les 14 registres du PSG)
+;         Y: poids faible adresse données sonores (14 octets pour les 14 registres du PSG)
+; 	adaoté de la routine en $FA86 pour l'Atmos - cf. L'Oric A Nu pp. 374-375
+_play_soundfx_entrypoint
+	php	; sauver registre d'état
+	sei	; et inhiber IRQ
+	stx $14 ; sauver adresse données dans (XY) dans ($14-$15)
+	sty $15 ;
+	ldy #00 ; indiquer paramètre 0
+load_psg_param_value:
+	lda ($14),y 		; prendre la valeur du paramètre de n° y 
+						; (l'adresse de départ des valeurs en en $14-$15, et on lui rajoute "y" pour 
+						; accéder à l'octet contenant la valeur du registre n° "y" à positionner
+	tax         		; sauve la valeur à positionner dans X
+	tya         		; sauver n° du registre dans A
+	pha 				; sauver A 'n° de registre)'
+	jsr set_psg_param 	; positionner le paramètre sur le PSG
+	pla 				; récupérer A (n° de registre)
+	tay 				; et le mettre dans Y
+	iny 				; et incrémenter y pour passer au n° de registre suivant
+	cpy #$0E 			; si Y vaut 14, c'est qu'on a transféré les 14 registres
+	bne load_psg_param_value
+	plp 				; FiN: récupérer registre d'état sauvegardé
+	rts 				; et sortir (NB: on a restauré le registre P, 
+						; donc l'état des interruptions (flag I) avant l'appel...)
+
+set_psg_param:
+; 	adapté de la routine en $F590 pour l'Atmos - cf. L'Oric A Nu pp. 345-346
+;   "ROUTINE D'E/S AVEC LE PSG 8912"
+php 					; sauver P car on va toucher à I
+sei 					; interdire IRQ (problèmes de timing) 
+						; -- NB: DEJA fait lors de l'appel routine $FA86 / _play_soundfx
+sta $030F 				; mettre le n° du registre à modifier sur le port
+						; E/S avec le VIA 6522
+tay 					; n° de registre dans Y
+txa 					; et valeur à positionner pour ce registre dans A
+cpy #$07 				; est-ce le registre d'autorisation ?
+bne skip_psg_auth_reg 	; non, sauter
+ora #$40 				; oui, forcer le port A en sortie
+skip_psg_auth_reg:
+pha 					; sauver la donnée sur la pile
+lda $30c
+ora #$EE 				; positionner CA2 et CB2 à 1
+sta $030c 				; indiquer n° de reigstre
+and #$11				; garder les sens de transition
+ora #$cc 				; positionner CA2 et CB2 à 0
+sta $030c 				; validation du n° de registre
+tax 					; on sauve la valeur de PCR dans X
+pla 					; on récupère la donnée
+sta $030f 				; et on la met sur le port
+txa
+ora #$ec 				; positionner CA2 à 1 et CB2 à 0
+sta $30c 				; indiquer donnée
+and #$11
+ora #$cc 				; positionner CA2 et CB2 à 0
+sta $030c 				; valider la donnée
+plp 					; récupérer registre d'état
+rts 					; et sortir
+
+
+; -- Routines de lecture individuelles (aucun intérêt)
 ; -- NOTE: routine utilisée pour Oric ATMOS $FA86: ENVOYER 14 PARAMETRES AU PSG AY-3-8912
 ;          (cf. L'Oric A Nu, pp. 374-375)
 ; -- NB:   pour l'Oric 1, l'adresse de cette routine est $FA6C
 
 _play_metalcrush_fx	
-	ldx #<metalcrush_fxtable
-	ldy #>metalcrush_fxtable
+	ldx #<_metalcrush_fxtable
+	ldy #>_metalcrush_fxtable
 	jmp $FA86
 
 _play_electricity_fx
-	ldx #<electricity_fxtable
-	ldy #>electricity_fxtable
+	ldx #<_electricity_fxtable
+	ldy #>_electricity_fxtable
 	jmp $FA86
 
 _play_helicopter_fx
-	ldx #<helicopter_fxtable
-	ldy #>helicopter_fxtable
+	ldx #<_helicopter_fxtable
+	ldy #>_helicopter_fxtable
 	jmp $FA86
 
 _play_helicopter2_fx
-	ldx #<helicopter2_fxtable
-	ldy #>helicopter2_fxtable
+	ldx #<_helicopter2_fxtable
+	ldy #>_helicopter2_fxtable
 	jmp $FA86
 
 _play_distant_helicopter_fx
-	ldx #<distant_helicopter_fxtable
-	ldy #>distant_helicopter_fxtable
+	ldx #<_distant_helicopter_fxtable
+	ldy #>_distant_helicopter_fxtable
 	jmp $FA86
 
 _play_engine_fx
-	ldx #<engine_fxtable
-	ldy #>engine_fxtable
+	ldx #<_engine_fxtable
+	ldy #>_engine_fxtable
 	jmp $FA86
 
 _play_planecockpit_fx
-	ldx #<planecockpit_fxtable
-	ldy #>planecockpit_fxtable
+	ldx #<_planecockpit_fxtable
+	ldy #>_planecockpit_fxtable
 	jmp $FA86
 
 _play_turboprop_fx
-	ldx #<turboprop_fxtable
-	ldy #>turboprop_fxtable
+	ldx #<_turboprop_fxtable
+	ldy #>_turboprop_fxtable
 	jmp $FA86
 
 _play_pong_fx
-	ldx #<pong_fxtable
-	ldy #>pong_fxtable
+	ldx #<_pong_fxtable
+	ldy #>_pong_fxtable
 	jmp $FA86
 
 _play_pchh_fx
-	ldx #<pchh_fxtable
-	ldy #>pchh_fxtable
+	ldx #<_pchh_fxtable
+	ldy #>_pchh_fxtable
 	jmp $FA86
--- main.c
+++ main.c
@@ -8,16 +8,17 @@
  *      Effets sonores et routine/définitions ASM par LadyWasky
  *      cf. http://forums.oric.org/t638-musique-en-c#8085
  *
- *      Version: v0.1
- *         Date: 15/01/19
+ *      Version: v0.2
+ *         Date: 16/01/19
  * 
  *      Historique:
  *      -----------------------------------------------------------------------------------------
- *      v0.1    - première version
+ *      v0.2WIP - BUGS !!
  *  =============================================================================================
  */ 
 
-#define VERSION_STRING  "v0.1 - retroric - 15/01/19"
+//#define VERSION_STRING  "v0.1 - retroric - 15/01/19"
+#define VERSION_STRING  "v0.2WIP -retroric-16/01/19"
 
 #include <stdio.h>
 #include <sys/graphics.h>
@@ -60,6 +61,7 @@ void  wait_spacekey();
 void  test_keys();
 void  hide_cursor();
 void  show_cursor();
+void  play_soundfx(const char *psg_data);
 
 /* ================== DECLARATION DES FONCTIONS ASSEMBLEUR (PROTOTYPES) ================== */
 extern void  play_metalcrush_fx();
@@ -73,6 +75,8 @@ extern void  play_planecockpit_fx();
 extern void  play_turboprop_fx();
 extern void  play_pong_fx();
 extern void  play_pchh_fx();
+extern void  play_soundfx_entrypoint();
+extern void  play_soundfx2(const char *psg_data);
 
 /* ================== VARIABLES GLOBALES ================== */
 
@@ -81,20 +85,99 @@ uchar *keyb_norm_key_press_addr = (uchar *) 0x208;
 
 // tableau de pointeurs vers des fonctions de type void sans arguments
 #define FXCOUNT 10
-void (*funcptrs[FXCOUNT])() = { play_metalcrush_fx,         play_electricity_fx,
-                                play_helicopter_fx,         play_helicopter2_fx, 
-                                play_distant_helicopter_fx, play_engine_fx, 
-                                play_planecockpit_fx,       play_turboprop_fx,
-                                play_pong_fx,               play_pchh_fx };
+void (*funcptrs[FXCOUNT])() = { 
+    play_metalcrush_fx,         play_electricity_fx,
+    play_helicopter_fx,         play_helicopter2_fx, 
+    play_distant_helicopter_fx, play_engine_fx, 
+    play_planecockpit_fx,       play_turboprop_fx,
+    play_pong_fx,               play_pchh_fx 
+};
 
 // tableau de chaines de caractères des noms des effets sonores
-char *soundfx_names[]       = { "metalcrush",         "electricity", 
-                                "helicopter",         "helicopter 2", 
-                                "distant helicopter", "engine", 
-                                "plane cockpit",      "turboprop",
-                                "pong",               "pchh" };
+const char *soundfx_names[] = { 
+    "metalcrush",         "electricity", 
+    "helicopter",         "helicopter 2", 
+    "distant helicopter", "engine", 
+    "plane cockpit",      "turboprop",
+    "pong",               "pchh" 
+};
+// données sonores
+
+
+extern const char   *metalcrush_fxtable,            *electricity_fxtable,
+                    *helicopter_fxtable,            *helicopter2_fxtable,
+                    *distant_helicopter_fxtable,    *engine_fxtable,
+                    *planecockpit_fxtable,          *turboprop_fxtable,
+                    *pong_fxtable,                  *pchh_fxtable;
+
+
+// COMMENTED OUT: doesn't compile in C ("initializer must be constant" error)
+// ==> REPLACED by _fx_data_vector_table in effects.s
+/*
+const char *soundfx_data[] = {   
+    metalcrush_fxtable,         electricity_fxtable, 
+    helicopter_fxtable,         helicopter2_fxtable, 
+    distant_helicopter_fxtable, engine_fxtable, 
+    planecockpit_fxtable,      turboprop_fxtable,
+    pong_fxtable,               pchh_fxtable 
+};
+*/
+
+extern const char *fx_data_vector_table[];
+
 
 /* ================== IMPLEMENTATION DES FONCTIONS ================== */
+
+void play_soundfx(const char *psg_data) {
+    // NB: utilisation de "STATIC" pour éviter que les variables locales hh et ll
+    // ne soient créées sur la pile, car cela interdiarait leur référencement dans
+    // l'inline asm (les vairables locales "auto"  étant créées sur la pile,
+    // cela conduirait à une traduction en ASM avec un adressage du type "ldx #(fp),6",
+    // qui n'est pas un adressage légal en 6502 car le framepointer "fp" n'est pas en page zéro... )
+    // NB: ne fonctionne toujours pas avec "static" ça se traduit par:
+    // "ldx #Lmain86;  ldy #Lmain85"
+    // BIZARREMENT, en redéclarant simplement "uchar hh, ll;" sans les initialiser au début,
+    // c'est "retombé en marche" pour l'asm "ldx #%ll; ldy #%hh;"" ??!!
+
+    //static uchar hh, ll;
+    uchar hh, ll;
+    /*
+    uchar i = 2;
+    printf("uchar i=%x\n", i); // Bizarrement avec %x un uchar s'affiche sur 4 digits au lieu de 2 !!??
+    */
+    hh = (uchar) ( (((unsigned int)  psg_data) >> 8)  & 0xff);
+    ll = (uchar) (  ((unsigned int)  psg_data)        & 0xff);
+    printf("playsoundfx: hh=%x, ll=%x\n", hh, ll);
+    
+    // OPTION 1: YIELDS following error:
+    //asm("ldx #%ll; ldy #%hh; jsr _play_soundfx_entrypoint");
+    /*  Assembling
+        ldx #(fp),6
+        V:\EMULATION\ORIC\OSDK\PROJECTS\SoundFXGallery\main.s(102):  093c:Syntax error
+        ldy #(fp),7
+        V:\EMULATION\ORIC\OSDK\PROJECTS\SoundFXGallery\main.s(103):  093c:Syntax error
+    */
+
+   /* See OSDK/documentation/doc_assembler.htm:
+
+    *       Before an expression you can use these unitary operators:
+    *           <  Gives the low byte of the expression
+    *           >  Gives the high byte
+    */
+    
+    // OPTION 2: used to yeild another error, but now ok ??!!
+    asm(    "pha; txa; pha; tya; pha;" // push registers (car la routine modifie A, X et Y)
+            //"ldx #<%psg_data; ldy #>%psg_data; "
+            //"ldx #$f7; ldy #$13;" // sound data addr for metalcrush
+            "ldx #%ll; ldy #%hh;" // finalement... c'est tombé en marche, avec variables locales ??!!
+            "jsr $FA86;" //"jsr _play_soundfx_entrypoint;"
+            "pla; tay; pla; tax; pla; " // pop saved registers
+
+        );
+    /*
+       <copy error message>
+    */
+}
 
 /**
  * main(): Point d'entrée du programme
@@ -102,19 +185,48 @@ char *soundfx_names[]       = { "metalcrush",         "electricity",
 void main() {
     uchar i;
 	//test_keys();
-
     cls(); paper(4); ink(2);
-
     hide_cursor();
-
     display_title_screen();
 
+    printf("----- STAGE 1: press SPACE -----\n");
+    wait_spacekey();
     for(i = 0; i < FXCOUNT; i++) {
         printf("Now playing: %s\n", soundfx_names[i]);
+        printf("routine addr = $%x\n", funcptrs[i]);
         printf("Press SPACE ");
         if(i < (FXCOUNT-1)) printf("for next sound FX\n\n");
         else                printf("to end\n\n");
         (*funcptrs[i])(); // appel de la fonction d'index i
+        wait_spacekey();
+    }
+
+/********* STAGE 2: fonction play_soundfx() buggée, sons bizarres !!! ***********/
+/**
+    printf("----- STAGE 2: press SPACE -----\n");
+    wait_spacekey();
+    printf("playsoundfx() addr = $%x\n\n", play_soundfx);
+    for(i = 0; i < FXCOUNT; i++) {
+        printf("Now playing: %s\n", soundfx_names[i]);
+        printf("fx data addr = $%x\n", fx_data_vector_table[i]);
+        play_soundfx(fx_data_vector_table[i]);
+        printf("Press SPACE ");
+        if(i < (FXCOUNT-1)) printf("for next sound FX\n\n");
+        else                printf("to end\n\n");
+        wait_spacekey();
+    }
+**/
+
+/********** STAGE 3: la fonction play_soundfx2() FONCTIONNE DESORMAIS !!! ************/
+    printf("----- STAGE 3: press SPACE -----\n");
+    printf("playsoundfx2() addr = $%x\n\n", play_soundfx2);
+    wait_spacekey();
+    for(i = 0; i < FXCOUNT; i++) {
+        printf("Now playing: %s\n", soundfx_names[i]);
+        printf("Press SPACE ");
+        play_soundfx2(fx_data_vector_table[i]);
+        if(i < (FXCOUNT-1)) printf("for next sound FX\n\n");
+        else                printf("to end\n\n");
         wait_spacekey();
     }
 
